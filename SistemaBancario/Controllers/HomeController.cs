d0462b717daad4aa22d3997db5b60312da9c50b7
Alexander <alexddlc@hotmail.es>
2020-03-19 15:09:09 -0400

    Contactanos

--- SistemaBancario/Controllers/HomeController.cs
+++ SistemaBancario/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace SistemaBancario.Controllers
             return View();
         }
 
+        public IActionResult Contactanos()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
