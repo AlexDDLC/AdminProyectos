b630eef26ea8753cd2dd7e550b46f272bd1f61ca
Alexander <alexddlc@hotmail.es>
2020-03-24 01:48:20 -0400

    Calcular cuotas

--- SistemaBancario/Controllers/PrestamosController.cs
+++ SistemaBancario/Controllers/PrestamosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SistemaBancario.Models;
 
 namespace SistemaBancario.Controllers
 {
@@ -12,9 +13,26 @@ namespace SistemaBancario.Controllers
         {
             return View();
         }
-        
+
         public IActionResult SolicitarPrestamo()
         {
+            var tipo_prestamos = new CalculadoraAmortizacion();
+            return View(tipo_prestamos);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SolicitarPrestamo(CalculadoraAmortizacion CalAm)
+        {
+            float CP_monto, CP_plazos, CP_interes;
+            CP_monto = CalAm.monto;
+            CP_plazos = CalAm.plazos;
+            CP_interes = CalAm.interes;
+            if(CalAm.calcular == "calcular")
+            {
+                CalAm.total = ((CP_monto*(CP_interes/CP_plazos))/100) + (CP_monto/CP_plazos);
+            }
+            ViewData["total"] = CalAm.total; 
             return View();
         }
 
