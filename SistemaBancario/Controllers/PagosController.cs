4702494e9dbf8b00c0ed7236662cc6e470238423
Master <Master@MASTER>
2020-03-29 23:39:02 -0400

    Model, Controller, views = Pagos

--- SistemaBancario/Controllers/PagosController.cs
+++ SistemaBancario/Controllers/PagosController.cs
@@ -10,40 +10,54 @@ namespace SistemaBancario.Controllers
     public class PagosController : Controller
     {
         ConPagos conPagos = new ConPagos();
+
         public IActionResult ListarPagos()
         {
             List<ListaPagos> ListPagos = new List<ListaPagos>();
             ListPagos = conPagos.listarPagos().ToList();
             return View(ListPagos);
         }
-        public IActionResult PagarPago(int idsol)
+        public IActionResult PagarPago(int idPag)
         {
-            ListaPagos lsp = ConPagos.ListaPagosID(idsol);
+            ListaPagos lsp = conPagos.listarPagosID(idPag);
             return View(lsp);
         }
 
         [HttpPost]
-        public IActionResult PagarPago(int idsol, [Bind] ListaPagos lsp)
+        public IActionResult PagarPago(int idPag, [Bind] ListaPagos lsp)
         {
             if (ModelState.IsValid)
             {
-                if (lsp.accion == "Pagar")
+                if (lsp.PorPagar == "Aprobado")
                 {
-                    lsp.NuevoEstadoPrestamo = "Aprobado";
+                    lsp.NuevoEstadoPago = "Aprobado";
                 }
-                else if (lsp.accion == "Rechazar")
+                else if (lsp.PorPagar == "Rechazar")
                 {
-                    lsp.NuevoEstadoPrestamo = "Rechazado";
+                    lsp.NuevoEstadoPago = "Rechazado";
                 }
                 DateTime fecha = DateTime.Now;
                 string formato = string.Format("{0:dd/MM/yyyy}", fecha);
-                lsp.fechaRehazo = formato;
-                conpres.ActualizarEstadoDePrestamo(lsp);
+                lsp.FechaPago = formato;
+                conPagos.ActualizarPagos(lsp);
                 return RedirectToAction("ListaSolicitudesPrestamos");
             }
-            return View(conpres);
+            return View(conPagos);
         }
 
+        public IActionResult ListaPagosPagados()
+        {
+            List<ListaPagos> listPagPa = new List<ListaPagos>();
+            listPagPa = conPagos.ListaPagosPagados().ToList();
+            return View(listPagPa);
+        }
+
+        public IActionResult ListaPagosSin()
+        {
+            List<ListaPagos> listPagSin = new List<ListaPagos>();
+            listPagSin = conPagos.ListaPagosSin().ToList();
+            return View(listPagSin);
+        }
         public IActionResult Index()
         {
             return View();
