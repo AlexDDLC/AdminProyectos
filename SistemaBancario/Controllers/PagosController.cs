59feaf269e0359f49bcca633de6f8667a7a4fd52
Master <Master@MASTER>
2020-03-29 20:48:56 -0400

    Model and Controller Pagos

--- SistemaBancario/Controllers/PagosController.cs
+++ SistemaBancario/Controllers/PagosController.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SistemaBancario.Models;
 
 namespace SistemaBancario.Controllers
 {
     public class PagosController : Controller
     {
+        ConPagos conPagos = new ConPagos();
+        public IActionResult ListarPagos()
+        {
+            List<ListaPagos> ListPagos = new List<ListaPagos>();
+            ListPagos = conPagos.listarPagos().ToList();
+            return View(ListPagos);
+        }
+        public IActionResult PagarPago(int idsol)
+        {
+            ListaPagos lsp = ConPagos.ListaPagosID(idsol);
+            return View(lsp);
+        }
+
+        [HttpPost]
+        public IActionResult PagarPago(int idsol, [Bind] ListaPagos lsp)
+        {
+            if (ModelState.IsValid)
+            {
+                if (lsp.accion == "Pagar")
+                {
+                    lsp.NuevoEstadoPrestamo = "Aprobado";
+                }
+                else if (lsp.accion == "Rechazar")
+                {
+                    lsp.NuevoEstadoPrestamo = "Rechazado";
+                }
+                DateTime fecha = DateTime.Now;
+                string formato = string.Format("{0:dd/MM/yyyy}", fecha);
+                lsp.fechaRehazo = formato;
+                conpres.ActualizarEstadoDePrestamo(lsp);
+                return RedirectToAction("ListaSolicitudesPrestamos");
+            }
+            return View(conpres);
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+
     }
 }
---
+++ SistemaBancario/Models/ConPagos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.Models
+{
+    public class ConPagos
+    {
+        ConSQL conexionSQL = new ConSQL();
+
+        public IEnumerable<ListaPagos> listarPagos()
+        {
+            List<ListaPagos> ListPagos = new List<ListaPagos>();
+
+            using (SqlConnection con = new SqlConnection(conexionSQL.conexion))
+            {
+                SqlCommand cmd = new SqlCommand("listaPagos", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    ListaPagos lsp = new ListaPagos();
+                    lsp.ID_Pago = dr["ID_Pago"].ToString();
+                    lsp.FK_ID_Cliente = dr["Cliente"].ToString();
+                    lsp.FK_ID_Prestamos = dr["Prestamo"].ToString();
+                    lsp.Monto = dr["Monto"].ToString();
+                    lsp.CuotasCubiertas = dr["CuotasCubiertas"].ToString();
+                    lsp.CuotasFaltantes = dr["CuotasFaltantes"].ToString();
+                    
+                    ListPagos.Add(lsp);
+                }
+                con.Close();
+            }
+            return ListPagos;
+        }
+    }
+}
