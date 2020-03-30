0293077a706ba82dd1b27b766bfed83551b90772
Antony Ventura <antony28.94@gmail.com>
2020-03-30 13:07:16 -0400

    Filtrado de clientesy empelados por cedula

---
+++ SistemaBancario/Models/BuscarUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.Models
+{
+    public class BuscarUsuario
+    {
+
+        public string cedula { set; get; }
+       
+    }
+}
